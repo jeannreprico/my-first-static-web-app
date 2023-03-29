49e7cb869fd8180dd2b498297e494102fa659f5b
jeannreprico <127900603+jeannreprico@users.noreply.github.com>
2023-03-29 14:10:19 -0500

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}, Gian que so pa con tigo?</div>`,
 })
 export class AppComponent {
   value = 'World';
